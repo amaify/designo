8d374db0d92b9db6a6765a3fec87162d061b381c
Tobechukwu Ugwuanyi <john.ugwuanyi@yahoo.com>
2021-04-05 23:47:59 +0100

    More updates

---
+++ index.js
@@ -0,0 +1,29 @@
+let menu = document.querySelector('.mobileNav-logo__menu');
+let navItems = document.querySelector('.mobileNav-link');
+let body = document.querySelector("body");
+
+let hamburger = `<img src="./assets/shared/mobile/icon-hamburger.svg" alt="Mobile Toggle Icon">`
+let closeMenu = `<img src="./assets/shared/mobile/icon-close.svg" alt="Mobile Toggle Icon">`
+
+window.onload = menu.innerHTML = hamburger;
+
+function openNav() {
+    body.classList.add("add-backdrop");
+    navItems.style.display = "block";
+
+    menu.innerHTML = closeMenu
+}
+
+function closeNav() {
+    body.classList.remove('add-backdrop');
+    navItems.style.display = "none";
+
+    menu.innerHTML = hamburger
+}
+
+menu.addEventListener('click', () => {
+    menu.innerHTML === hamburger ? openNav() : closeNav()
+})
+
+// console.log(body);
+
